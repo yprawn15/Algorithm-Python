5c2ac88a9374835b51627dd2adc7804f7906f806
yshrimp <eogksla123@gmail.com>
2021-03-24 23:25:04 +0900

    unit8

---
+++ grammar/unit8.py
@@ -0,0 +1,16 @@
+# 8 불(bool)과 비교, 논리 연산자
+
+# bool은 True, False로 표현하며, 1, 3, 5, 'python' 처럼 값의 일종이다.
+
+print(3 > 1)
+
+print(10 == 10)
+
+print(10 != 5) # != 같지 않다는 부호
+
+# 8.1 is와 is not
+print(1 == 1.0)
+
+print(1 is 1.0)
+
+print(1 is not 1.0)
