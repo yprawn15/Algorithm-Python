cf2c4c080fd8ea13e2c9a2717bd2f97af7437e24
yshrimp <eogksla123@gmail.com>
2021-03-24 23:36:35 +0900

    unit9

--- grammar/unit8.py
+++ grammar/unit8.py
@@ -14,3 +14,15 @@ print(1 == 1.0)
 print(1 is 1.0)
 
 print(1 is not 1.0)
+
+# == is의 차이 ==은 값 자체만을 is는 1, 1.0의 자료형까지 비교했기 때문
+# 값 비교에 is를 사용하지 말자
+
+# 8.2 논리 연산자(and, or, not)
+print(True and True)
+print(True and False) # 무조건 둘 다 True여야 결과가 True
+
+print(True or False) # 하나만 True를 충족시켜도 True
+
+print(not True)
+print(not False) # not은 논릿값을 뒤집는다.
---
+++ grammar/unit9.py
@@ -0,0 +1,26 @@
+# 9 문자열 사용하기
+
+hello = '''Hello, World!
+안녕하세요
+Python입니다.'''
+
+print(hello)
+#여러줄인 변수 할당 법 ''', """"
+
+single_quote = '''"안녕하세요."
+'파이썬'입니다.'''
+
+double_quote1 = """"Hello"
+'Python'"""
+
+double_quote2 = """hello, 'Python'"""
+
+print(single_quote)
+print(double_quote1)
+print(double_quote2)
+
+#참고 문자열에 따움표 포함하는 방법
+'Python isn\'t difficult'
+# 따옴표 앞에 \붙이기
+
+print('Hello\nPython')
