6c5c3a8309dae96ceccae3a9400eaa1e0ba38f73
yshrimp <yprawn15@gmail.com>
2021-08-12 20:50:33 +0900

    Backjoon 2562 최댓값

---
+++ backjoon/2562-최댓값.py
@@ -0,0 +1,12 @@
+arr = []
+for i in range(9):
+  arr += list(map(int, input().split())))
+
+print(max(arr))
+print(arr.index(max(arr)) + 1)
+
+  
+# 궁금점 
+# map(int, input().split())은 되고, int(input())은 안됨
+
+
