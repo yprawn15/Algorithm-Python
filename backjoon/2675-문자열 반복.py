f4da0cba452afe0e3e2714247fda160f746896ec
yprawn15 <yprawn15@gmail.com>
2021-08-19 20:32:00 +0900

    Baekjoon 2675 문자열 반복

--- backjoon/2675-문자열 반복.py
+++ backjoon/2675-문자열 반복.py
@@ -5,4 +5,15 @@ for i in range(t):
     print(*sorted(r*int(q)), sep='')
 
 # alphanumeric 문자  0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./:
-# 에러 1. 0DE$ 일때, $먼저 나옴; 
+# 에러 1. 0DE$ 일때, $먼저 나옴;
+
+
+# 2
+t = int(input())
+
+for i in range(t):
+    r, s = input().split()
+    text = ''
+    for j in s:
+        text += int(r)*j
+    print(text)
