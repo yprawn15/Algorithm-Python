324c11ebb79e57e9c33b3aa5255b6c483addb591
yprawn15 <yprawn15@gmail.com>
2021-12-11 17:44:47 +0900

    baekjoon 2775 부녀회장

--- backjoon/8/2775-부녀회장이 퇼테야.py
+++ backjoon/8/2775-부녀회장이 퇼테야.py
@@ -1,8 +1,13 @@
 t = int(input())
 
-
-
-for i in range(t):
+for _ in range(t):
     k = int(input())
     n = int(input())
+    f0 = list(range(1, n+1)) # [1, 2, 3, 4, ....., n] n개
     
+    for i in range(k):
+        for j in range(1, n): # 1, 2, 3, 4, .... n-1
+            f0[j] += f0[j-1]  
+    print(f0[-1])
+
+# f0[j] += f0[j-1] <<< 이 생각이 상당히 히트
--- backjoon/note.py
+++ backjoon/note.py
@@ -1,4 +1,8 @@
-x = 5
-for i in range(2):
-    x -=1
-print(x)
+f0 = list(range(1, 11)) #[1, 2, 3, 4,... 10]
+
+f1 = [0]*10
+
+for i in range(10):
+    f1[i] += sum(f0[0:i+1])
+
+print(f1)
