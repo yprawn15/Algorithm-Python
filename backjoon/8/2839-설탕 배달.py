538f8daf83ce9988396f8e790037eeeb91a13a77
yprawn15 <yprawn15@gmail.com>
2021-12-11 18:10:40 +0900

    beakjoon 2839 설탕배달

--- backjoon/8/2839-설탕 배달.py
+++ backjoon/8/2839-설탕 배달.py
@@ -1,12 +1,14 @@
-sugar = int(input())
+n = int(input())
 
-bag = 0
-while sugar >= 0 :
-    if sugar % 5 == 0 :  # 5의 배수이면
-        bag += (sugar // 5)  # 5로 나눈 몫을 구해야 정수가 됨
-        print(bag)
+cnt = 0
+
+while n >= 0:
+    if n % 5 == 0:
+        print(cnt + (n//5))
         break
-    sugar -= 3  
-    bag += 1  # 5의 배수가 될 때까지 설탕-3, 봉지+1
-else :
+    n -= 3
+    cnt += 1
+else: 
     print(-1)
+        
+# 의문 else를 저렇게 써도 되는가
