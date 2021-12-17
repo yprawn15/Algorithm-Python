a03ae1dca6ba21a3035421591e682e4c7a6b8917
yprawn15 <yprawn15@gmail.com>
2021-12-17 21:12:38 +0900

    baekjoon 3053 택시 기하학

---
+++ backjoon/9/3053-택시 기하학.py
@@ -0,0 +1,8 @@
+r = int(input())
+
+import math
+print(round((math.pi)*(r**2), 6))
+print(f'{2*(r**2)}.000000')
+
+# 파이를 사용하고 싶을때 import math로 math.pi를 불러 pi 함수를 사용함
+# 택시 기하학은 주로 맨해튼 거리라 불리는데 이때 원의 정의가 정사각형임
