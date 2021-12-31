8786971c207205a14462ca54681f3f397259cd29
yprawn15 <yprawn15@gmail.com>
2021-12-31 22:46:06 +0900

    boj 1018 체스판

--- backjoon/11/1018-체스판.py
+++ backjoon/11/1018-체스판.py
@@ -1,5 +1,3 @@
-# n, m = map(int, input().split())
-
-a = list(input())
+a = 'BBWBWBWB'
 
 print(a[1::2])
--- backjoon/exercise.py
+++ backjoon/exercise.py
@@ -1,24 +1,3 @@
-def get_stars(t):
-    matrix = []
-    for i in range(3 * len(t)): # (0 ~ 8)
-        if i // len(t) == 1: # i == 3
-            matrix.append(t[i % len(t)] + ' ' * len(t) + t[i % len(t)])
-        else:
-            matrix.append(t[i % len(t)] * 3)
-    return matrix
+a = ['BBWBWBWBWB', 'fadsfa']
 
-
-
-star = ['***', '* *', '***']
-n = int(input())
-e = 0
-while n != 3:
-    n = int(n / 3)
-    e += 1
-
-for i in range(e):
-    star = get_stars(star)
-for i in star:
-    print(i)
-
-print(star)
+print(a[1][0])
